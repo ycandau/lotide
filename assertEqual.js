66e25ebcd8ea6b809419317fcf5436a3c8ac9d22
Yves Candau <ycandau@gmail.com>
2021-06-21 21:10:54 +0000

    Formatted with prettier

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,14 @@
 const assertEqual = function(actual, expected) {
-
   const [color, emoji, outcome, operator] =
     actual === expected
       ? ['\x1b[2m\x1b[32m', '  ', 'Passed', '===']
       : ['\x1b[0m\x1b[31m', '\u274c', 'Failed', '!=='];
 
-  console.log(color, emoji,
-    `Assertion ${outcome}: ${actual} ${operator} ${expected}`);
+  console.log(
+    color,
+    emoji,
+    `Assertion ${outcome}: ${actual} ${operator} ${expected}`
+  );
 };
 
 // Tests
