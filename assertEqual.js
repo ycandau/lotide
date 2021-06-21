bf392e8fe002eb126c5adfc5c32ecc748674fbed
Yves Candau <ycandau@gmail.com>
2021-06-21 21:05:10 +0000

    Added assertEqual()

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+// Function definition
+const assertEqual = function(actual, expected) {
+
+  const [color, emoji, outcome, operator] =
+    actual === expected
+      ? ['\x1b[2m\x1b[32m', '  ', 'Passed', '===']
+      : ['\x1b[0m\x1b[31m', '\u274c', 'Failed', '!=='];
+
+  console.log(color, emoji,
+    `Assertion ${outcome}: ${actual} ${operator} ${expected}`);
+};
+
+// Tests
+assertEqual("Same", "Same");
+assertEqual("Same", "Different");
+assertEqual(1, 1);
+assertEqual(1, 2);
