aa38f1eed219ab29fe3b8a433e33b796e643499e
Yves Candau <ycandau@gmail.com>
2021-06-21 21:50:19 +0000

    Added head()

---
+++ head.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  const [color, emoji, outcome, operator] =
+    actual === expected
+      ? ['\x1b[2m\x1b[32m', '  ', 'Passed', '===']
+      : ['\x1b[0m\x1b[31m', '\u274c', 'Failed', '!=='];
+
+  console.log(
+    color,
+    emoji,
+    `Assertion ${outcome}: ${actual} ${operator} ${expected}`
+  );
+};
+
+const head = (array) => array[0];
+
+// Tests
+assertEqual(head([]), undefined);
+assertEqual(head([null]), null);
+assertEqual(head([1]), 1);
+assertEqual(head([1, 2, 3]), 1);
+assertEqual(head(['a']), 'a');
+assertEqual(head(['a', 'b', 'c']), 'a');
+
