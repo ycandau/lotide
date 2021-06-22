47c2ff5d3d8f44d80cab2e1a3bc11887b3917450
Yves Candau <ycandau@gmail.com>
2021-06-22 13:47:57 -0700

    Removed unused assertion functions in countOnly()

--- countOnly.js
+++ countOnly.js
@@ -10,23 +10,6 @@ const assertEqual = function (actual, expected) {
   );
 };
 
-const eqArrays = (arr1, arr2) => {
-  return (
-    arr1.length === arr2.length &&
-    arr1.reduce((same, elem, index) => same && elem === arr2[index], true)
-  );
-};
-
-const assertArraysEqual = (actual, expected) => {
-  const [color, emoji, outcome, operator] = eqArrays(actual, expected)
-    ? ['\x1b[2m\x1b[32m', '  ', 'Passed', '===']
-    : ['\x1b[0m\x1b[31m', '\u274c', 'Failed', '!=='];
-  console.log(
-    color + emoji + `Assertion ${outcome}:`,
-    `[${actual.join(', ')}] ${operator} [${expected.join(', ')}]`
-  );
-};
-
 // Actual function
 const countOnly = (array, itemsToCount) => {
   const counts = {};
