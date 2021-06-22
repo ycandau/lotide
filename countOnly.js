7245bafc50a147b03034d0d88b289807379ec781
Yves Candau <ycandau@gmail.com>
2021-06-22 14:03:31 -0700

    Added countLetters()

--- countOnly.js
+++ countOnly.js
@@ -1,5 +1,5 @@
 // Test assertion functions
-const assertEqual = function (actual, expected) {
+const assertEqual = (actual, expected) => {
   const [color, emoji, outcome, operator] =
     actual === expected
       ? ['\x1b[2m\x1b[32m', '  ', 'Passed', '===']
