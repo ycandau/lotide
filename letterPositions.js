b5974645d598ec358c66e6f2c3200e34812bdd45
Yves Candau <ycandau@gmail.com>
2021-06-23 15:53:36 -0700

    Added takeUntil()

--- letterPositions.js
+++ letterPositions.js
@@ -20,7 +20,6 @@ const assertArraysEqual = (arr1, arr2) => {
   const [color, emoji, outcome, operator] = eqArrays(arr1, arr2)
     ? ['\x1b[2m\x1b[32m', '  ', 'Passed', '===']
     : ['\x1b[0m\x1b[31m', '\u274c', 'Failed', '!=='];
-
   console.log(
     color + emoji + `Assertion ${outcome}:`,
     `[${arr1.join(', ')}] ${operator} [${arr2.join(', ')}]`
