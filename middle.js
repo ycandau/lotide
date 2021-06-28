11303fd2f9eb2f4f9a2280413c83e00800e05676
Yves Candau <ycandau@gmail.com>
2021-06-28 12:40:13 -0700

    Refactored middle to export the function and separate the testing

--- middle.js
+++ middle.js
@@ -1,22 +1,3 @@
-// Test assertion functions
-const eqArrays = (arr1, arr2) => {
-  return (
-    arr1.length === arr2.length &&
-    arr1.reduce((same, elem, index) => same && elem === arr2[index], true)
-  );
-};
-
-const assertArraysEqual = (actual, expected) => {
-  const [color, emoji, outcome, operator] = eqArrays(actual, expected)
-    ? ['\x1b[2m\x1b[32m', '  ', 'Passed', '===']
-    : ['\x1b[0m\x1b[31m', '\u274c', 'Failed', '!=='];
-  console.log(
-    color + emoji + `Assertion ${outcome}:`,
-    `[${actual.join(', ')}] ${operator} [${expected.join(', ')}]`
-  );
-};
-
-// Actual function
 const middle = (array) => {
   const middle = array.length >> 1;
   return array.length <= 2
@@ -26,9 +7,4 @@ const middle = (array) => {
     : [array[middle - 1], array[middle]];
 };
 
-// Tests
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+module.exports = middle;
